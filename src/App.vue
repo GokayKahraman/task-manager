6069fd7aaa8eed72956ad2b46690d4a2c7ce5e9a
gokay <gokay.kahraman@yandex.com>
2021-03-24 23:41:37 +0300

    refactor edildi

--- src/App.vue
+++ src/App.vue
@@ -1,111 +1,110 @@
 <template>
-<div>
-  <div class="text">
-
-    <div class="style" style="margin-top: 168px;">Hi there! Welcome to</div>
-      <img src="./assets/image.png" style="margin-top: 24px;"/>
-      <div class="style" style="margin-top: 94px;">Very simple Things To-Do List. Helps you to<br>manage yourdaily life, without any hassle!</div>
+  <div class="main">
+    <div style="margin-top: 168px;">Hi there! Welcome to</div>
+    <img src="./assets/image.png" style="margin-top: 24px;" />
+    <div style="margin-top: 94px;">
+      Very simple Things To-Do List. Helps you to<br />manage yourdaily life,
+      without any hassle!
     </div>
 
     <div class="footer">
-      <a href="https://www.instagram.com/gokykh_/">
-        <div class="link facebook">
-                
-          <div class="logo darkblue"></div>
-          <div class="linkintext">FACEBOOK</div>                
-        </div>
-      </a>
-        
-      <a href="https://twitter.com/GokayKh">
-        <div class="link twitter" style="margin-top: 15px;">
-               
-          <div class="logo blue"></div>
-          <div class="linkintext">TWITTER</div>
-                
-        </div>
-      </a>
-    </div>   
-</div>
+      <div class="social-container facebook">
+        <a class="social-link" href="https://www.instagram.com/gokykh_/">
+          <div class="facebook-logo"></div>
+          <div class="social-title">FACEBOOK</div>
+        </a>
+      </div>
+
+      <div class="social-container twitter" style="margin-top: 15px;">
+        <a class="social-link" href="https://twitter.com/GokayKh">
+          <div class="twitter-logo"></div>
+          <div class="social-title">TWITTER</div>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
+//js
 </script>
 
 <style lang="scss" scoped>
- @font-face {
-    font-family: "Raleway";
-    src: url("./assets/Raleway-Regular.ttf");
+@font-face {
+  font-family: "Raleway";
+  src: url("./assets/Raleway-Regular.ttf");
+}
+.main {
+  text-align: center;
+  font-family: Raleway;
+  font-style: normal;
+
+  font-weight: normal;
+  font-size: 14px;
+
+  color: #050505;
 }
 
 .text {
-    text-align: center;
-}
-
-.style {
-    font-family: Raleway;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 14px;
-
-    color: #050505;
-
+  text-align: center;
 }
 
 .footer {
-    margin-top: 145px;
-    margin-left: 48px;
+  margin-top: 145px;
 }
 
-.link {
-    display: flex;
-
-    width: 280px;
-    height: 50px;
-    border-radius: 3px;
-    color: white;
+.social-container {
+  width: 280px;
+  height: 50px;
+  border-radius: 3px;
+  color: white;
+  margin: auto;
 }
 
-.logo {
-    width: 50px;
-    height: 50px;
-    border-radius: 3px;
+.facebook-logo {
+  width: 50px;
+  height: 50px;
+  border-radius: 3px;
+
+  background-color: #283b68;
+  background-image: url("./assets/face.png");
+  background-repeat: no-repeat;
+  background-position: center;
 }
 
-.darkblue {
-    background-color: #283B68;
-    background-image: url("./assets/face.png");
-    background-repeat:no-repeat;
-    background-position:center;
+.twitter-logo {
+  width: 50px;
+  height: 50px;
+  border-radius: 3px;
+
+  background-color: #51b2e0;
+  background-image: url("./assets/twit.png");
+  background-repeat: no-repeat;
+  background-position: center;
 }
 
-.blue {
-    background-color: #51B2E0;
-    background-image: url("./assets/twit.png");
-    background-repeat:no-repeat;
-    background-position:center;
-}
-.linkintext {
-    margin-left: 23px;
-    margin-top: 19px;
+.social-title {
+  margin-left: 23px;
+  margin-top: 19px;
 
-    font-family: Raleway;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 11px;
-    line-height: 13px;
+  font-family: Raleway;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 11px;
+  letter-spacing: 1px;
 
-
-    letter-spacing: 1px;
-
-    color: #FFFFFF;
+  color: #ffffff;
 }
 
 .facebook {
-    background-color: #3B5798;
+  background-color: #3b5798;
 }
 
 .twitter {
-    background-color: #59DCFF;
+  background-color: #59dcff;
+}
+.social-link {
+  display: flex;
+  text-decoration: none;
 }
 </style>
